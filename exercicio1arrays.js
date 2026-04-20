ab6ba05b2dcddbaebc8009e61e76c2623810a350
lizaraxamg@proton.me <lizaraxamg@proton.me>
2026-04-20 15:22:10 -0300

    Salvando lógica do laço for para percorrer lista

---
+++ exercicio1arrays.js
@@ -0,0 +1,4 @@
+const numero = 5;
+for(let i = 1; i <= 10; index++){
+  console.log(i*numero)
+}
---
+++ exercicio3arrrays.js
@@ -0,0 +1,10 @@
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let i  = 0; i < numeros.Length; i++){
+    const numerosPares  = numeros[i] ;
+    console.log(numeros);
+    if (numero % 2 === 0) {
+        console.log(numeros);
+    }
+
+}
+    
